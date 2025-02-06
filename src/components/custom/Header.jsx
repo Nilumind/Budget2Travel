5c82f64648ea0935fd1c82ccb66539febf3521ad
Ansala <ansalagunawardana@gmail.com>
2025-02-06 06:51:57 +0530

    first commit

---
+++ src/components/custom/Header.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react'
+import { Button } from '../ui/button'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import { useNavigation } from 'react-router-dom';
+import { FcGoogle } from "react-icons/fc";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import axios from 'axios';
+function Header() {
+
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  const [openDailog, setOpenDailog] = useState(false);
+  
+  useEffect(() => {
+    console.log(user);
+  }, []);
+  
+  const login = useGoogleLogin({
+    onSuccess: (codeResp) => GetUserProfile(codeResp),
+    onError: (error) => console.log(error),
+  });
+  
+  const GetUserProfile = async (tokenInfo) => {
+    try {
+      console.log("HERE", tokenInfo);
+      const resp = await axios.get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${tokenInfo?.access_token}`,
+            Accept: 'Application/json',
+          },
+        }
+      );
+      console.log(resp);
+  
+      if (!localStorage.getItem('user')) {
+        localStorage.setItem('user', JSON.stringify(resp.data));
+        setOpenDailog(false);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+    }
+  };
+  
+
+  return (
+    <div className='p-3 shadow-sm flex justify-between items-center px-5'>
+     <a href='/'>
+      <img width="170" height="41" src='/mainlogo.png' />
+      </a>
+      <div>
+        {user ?
+          <div className='flex items-center gap-3'>
+            <a href='/create-trip'>
+            <Button variant="outline" 
+            className="rounded-full">+ Create Trip</Button>
+            </a>
+            <a href='/my-trips'>
+            <Button variant="outline" 
+            className="rounded-full">My Trips</Button>
+            </a>
+            <Popover>
+            <PopoverTrigger>
+            <img src={user?.picture} className='h-[35px] w-[35px] rounded-full'/>
+            </PopoverTrigger>
+            <PopoverContent>
+              <h2 className='cursor-pointer' onClick={()=>{
+                googleLogout();
+                localStorage.clear();
+                window.location.reload();
+              }}>Logout</h2>
+            </PopoverContent>
+          </Popover>
+          </div>
+          :
+          <Button onClick={()=>setOpenDailog(true)}>Sign In</Button>
+        }
+      </div>
+      <Dialog open={openDailog}>
+
+<DialogContent>
+  <DialogHeader>
+
+    <DialogDescription>
+      <img src="/logo.svg" />
+      <h2 className='font-bold text-lg mt-7'>Sign In With Google</h2>
+      <p>Sign in to the App with Google authentication securely</p>
+
+      <Button
+
+        onClick={login}
+        className="w-full mt-5 flex gap-4 items-center">
+
+        <FcGoogle className='h-7 w-7' />
+        Sign In With Google
+
+      </Button>
+
+    </DialogDescription>
+  </DialogHeader>
+</DialogContent>
+</Dialog>
+    </div>
+  )
+}
+
+export default Header
